b9c01bbadfa154ff30e5d32b38cf6316b6a04706
oxoxoxoxox <dudwlsaos132@naver.com>
2024-09-19 15:29:57 +0900

    [add] 판매자에 해당하는 MVC 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SellersController } from './sellers/sellers.controller';
+import { SellersService } from './sellers/sellers.service';
+import { SellersModule } from './sellers/sellers.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [SellersModule],
+  controllers: [AppController, SellersController],
+  providers: [AppService, SellersService],
 })
 export class AppModule {}
