2b71844bcb306d22617fd850c725681a279cd5fe
oxoxoxoxox <dudwlsaos132@naver.com>
2024-10-01 01:11:15 +0900

    [add] 소비자 회원가입 로직 추가

--- src/customer/customer.controller.ts
+++ src/customer/customer.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CustomerService } from './customer.service';
+import { CustomerRegistDto } from './dto/customer.regist.dto';
 
 @Controller('customer')
-export class CustomerController {}
+export class CustomerController {
+  constructor(private readonly customerService: CustomerService) {}
+
+  @Post('regist')
+  async regist(@Body() body: CustomerRegistDto) {
+    return this.customerService.regist(body);
+  }
+}
--- src/customer/customer.service.ts
+++ src/customer/customer.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CustomerEntity } from '../Entity/customer.entity';
+import { Repository } from 'typeorm';
+import { CustomerRegistDto } from './dto/customer.regist.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class CustomerService {}
+export class CustomerService {
+  constructor(
+    @InjectRepository(CustomerEntity)
+    private readonly customerEntity: Repository<CustomerEntity>,
+  ) {}
+  async regist(body: CustomerRegistDto) {
+    const data: CustomerEntity = new CustomerEntity();
+    data.name = body.name;
+    data.userID = body.userID;
+    data.userPW = await bcrypt.hash(body.userPW, 10);
+    data.address = body.address;
+    data.email = body.email;
+
+    const save: CustomerEntity = await this.customerEntity.save(data);
+    return save;
+  }
+}
