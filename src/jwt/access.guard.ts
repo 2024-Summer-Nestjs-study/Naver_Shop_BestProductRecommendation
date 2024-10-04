79af024d5adc3323fa480168a8ecb068596ce072
oxoxoxoxox <dudwlsaos132@naver.com>
2024-10-04 17:41:26 +0900

    [edit] guard error message 수정

--- src/jwt/access.guard.ts
+++ src/jwt/access.guard.ts
@@ -1,9 +1,4 @@
-import {
-  CanActivate,
-  ExecutionContext,
-  Injectable,
-  UnauthorizedException,
-} from '@nestjs/common';
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
 
@@ -16,12 +11,12 @@ export class AccessGuard implements CanActivate {
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const req = context.switchToHttp().getRequest();
-    const token = req.header.authorization;
+    const token = req.headers.authorization;
     if (!token) {
       throw new UnauthorizedException('헤드가 비었습니다.');
     }
     const jwt = token.replace('Bearer ', '');
-    const secretA = this.configService.get('access_Key');
+    const secretA = this.configService.get<string>('access_Key');
     try {
       const payload = await this.jwtService.verifyAsync(jwt, {
         secret: secretA,
@@ -29,14 +24,15 @@ export class AccessGuard implements CanActivate {
       req['user'] = payload;
     } catch (e) {
       if (e.name === 'JsonWebTokenError') {
-        throw new UnauthorizedException('accessToken Error');
+        throw new UnauthorizedException('access 아닌데?');
       }
       if (e.name === 'TokenExpiredError') {
         throw new UnauthorizedException(
           '만료되었습니다. 토큰을 다시 발급 받으시오.',
         );
       }
-      throw new Error();
+      console.log(e);
+      throw new UnauthorizedException();
     }
     return true;
   }
